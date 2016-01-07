d3e89e005e12d5db89be42b06583912ab6ce9e25
Alejandro Awesome <sokolkoff@gmail.com>
2016-01-07 13:56:09 +0300

    start and stop buttons merged to one added icons for buttons

--- app/src/main/java/org/kukish/android/stopwatch/StopWatchActivity.java
+++ app/src/main/java/org/kukish/android/stopwatch/StopWatchActivity.java
@@ -1,9 +1,11 @@
 package org.kukish.android.stopwatch;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class StopWatchActivity extends Activity {
@@ -12,28 +14,48 @@ public class StopWatchActivity extends Activity {
     //indicates stopwatch running or not
     private boolean running;
 
+    private Button startPauseButton;
+    private Drawable startIcon;
+    private Drawable pauseIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        startPauseButton = (Button) findViewById(R.id.start_pause_button);
+        startIcon = getResources().getDrawable(R.drawable.start);
+        pauseIcon = getResources().getDrawable(R.drawable.pause);
+
         if (savedInstanceState != null) {
             ticks = savedInstanceState.getInt("ticks");
             running = savedInstanceState.getBoolean("running");
+            if (running) {
+                startPauseButton.setText(R.string.pasue);
+                startPauseButton.setCompoundDrawablesWithIntrinsicBounds(null , null, pauseIcon, null);
+            } else if (ticks > 0) {
+                startPauseButton.setText(R.string.resume);
+            }
         }
         runTimer();
     }
 
-    public void onClickStart(View view) {
-        running = true;
-    }
-
-    public void onClickStop(View view) {
-        running = false;
+    public void onClickStartPause(View view) {
+        if (running) {
+            running = false;
+            startPauseButton.setText(ticks > 0 ? R.string.resume : R.string.pasue);
+            startPauseButton.setCompoundDrawablesWithIntrinsicBounds(null, null, startIcon, null);
+        } else {
+            running = true;
+            startPauseButton.setText(R.string.pasue);
+            startPauseButton.setCompoundDrawablesWithIntrinsicBounds(null, null, pauseIcon, null);
+        }
     }
 
     public void onClickReset(View view) {
         running = false;
         ticks = 0;
+        startPauseButton.setText(R.string.start);
+        startPauseButton.setCompoundDrawablesWithIntrinsicBounds(null, null, startIcon, null);
     }
 
     private void runTimer() {
